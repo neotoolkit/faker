6deb2ac318a6fc8ebf394b713dc81aeeac689923
Sasha Melentyev <sasha@melentyev.io>
2022-06-26 16:11:26 +0300

    feat: add address concept

--- faker.go
+++ faker.go
@@ -3,6 +3,7 @@ package faker
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -21,9 +22,45 @@ func NewFaker() Faker {
 	}
 }
 
+// Address returns Address instance
+func (f Faker) Address() Address {
+	return Address{
+		Faker:          &f,
+		postCodeFormat: []string{"*****", "******", "*****-****"},
+		country: []string{
+			"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica (the territory South of 60 deg S)", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
+			"Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island (Bouvetoya)", "Brazil", "British Indian Ocean Territory (Chagos Archipelago)", "British Virgin Islands", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi",
+			"Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Cote d\"Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic",
+			"Denmark", "Djibouti", "Dominica", "Dominican Republic",
+			"Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia",
+			"Faroe Islands", "Falkland Islands (Malvinas)", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern Territories",
+			"Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana",
+			"Haiti", "Heard Island and McDonald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary",
+			"Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy",
+			"Jamaica", "Japan", "Jersey", "Jordan",
+			"Kazakhstan", "Kenya", "Kiribati", "Korea", "Korea", "Kuwait", "Kyrgyz Republic",
+			"Lao People\"s Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg",
+			"Macao", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar",
+			"Namibia", "Nauru", "Nepal", "Netherlands Antilles", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway",
+			"Oman",
+			"Pakistan", "Palau", "Palestinian Territories", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico",
+			"Qatar",
+			"Reunion", "Romania", "Russian Federation", "Rwanda",
+			"Saint Barthelemy", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Martin", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard & Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic",
+			"Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu",
+			"Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "United States Minor Outlying Islands", "United States Virgin Islands", "Uruguay", "Uzbekistan",
+			"Vanuatu", "Venezuela", "Vietnam",
+			"Wallis and Futuna", "Western Sahara",
+			"Yemen",
+			"Zambia", "Zimbabwe",
+		},
+		buildingNumberFormat: []string{"*", "*/*"},
+	}
+}
+
 // Boolean returns Boolean instance
 func (f Faker) Boolean() Boolean {
-	return Boolean{&f}
+	return Boolean{Faker: &f}
 }
 
 // Internet returns Internet instance
@@ -173,7 +210,7 @@ func (f Faker) Person() Person {
 
 // UUID returns UUID instance
 func (f Faker) UUID() UUID {
-	return UUID{&f}
+	return UUID{Faker: &f}
 }
 
 // IntBetween returns a Int between a given minimum and maximum values
@@ -197,9 +234,28 @@ func (f Faker) RandomStringElement(s []string) string {
 func (f Faker) Asciify(in string) string {
 	var out strings.Builder
 
-	for i := 0; i < len(in); i++ {
+	out.Grow(len(in))
+
+	for i := range in {
 		if in[i] == '*' {
 			out.WriteString(fmt.Sprintf("%c", f.IntBetween(97, 122)))
+		} else {
+			out.WriteByte(in[i])
+		}
+	}
+
+	return out.String()
+}
+
+// Numerify returns a fake string that replace all "*" characters with numbers from 0 to 9 as string for Faker
+func (f Faker) Numerify(in string) string {
+	var out strings.Builder
+
+	out.Grow(len(in))
+
+	for i := range in {
+		if in[i] == '*' {
+			out.WriteString(strconv.Itoa(f.IntBetween(1, 9)))
 		} else {
 			out.WriteByte(in[i])
 		}
