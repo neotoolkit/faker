f8c203888aaab85545b90d574ac41c8e7026cf68
Sasha Melentyev <sasha@melentyev.io>
2022-06-26 11:50:46 +0300

    fix: fix lint issue

--- uuid.go
+++ uuid.go
@@ -14,8 +14,13 @@ type UUID struct {
 // V4 returns UUID V4 as string
 func (u UUID) V4() string {
 	var uuid [16]byte
-	io.ReadFull(rand.Reader, uuid[:])
+
+	if _, err := io.ReadFull(rand.Reader, uuid[:]); err != nil {
+		panic(err)
+	}
+
 	uuid[6] = (uuid[6] & 0x0f) | 0x40 // Version 4
 	uuid[8] = (uuid[8] & 0x3f) | 0x80 // Variant is 10
+
 	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
 }
