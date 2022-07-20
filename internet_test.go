645e65e4ed0fcfa37f4f1ef760bd4070950c90d2
Sasha Melentyev <sasha@melentyev.io>
2022-07-20 13:35:56 +0300

    feat: add HTTP status code

--- internet_test.go
+++ internet_test.go
@@ -25,3 +25,20 @@ func TestHTTPMethod(t *testing.T) {
 		t.Error("HTTP method is empty")
 	}
 }
+
+func TestFaker_HTTPStatusCode(t *testing.T) {
+	t.Parallel()
+	f := faker.New(faker.WithHTTPStatusCodes(200))
+	httpStatusCode := f.HTTPStatusCode()
+	if httpStatusCode != 200 {
+		t.Errorf("got %v, want 200", httpStatusCode)
+	}
+}
+
+func TestHTTPStatusCode(t *testing.T) {
+	t.Parallel()
+	httpStatusCode := faker.HTTPStatusCode()
+	if httpStatusCode == 0 {
+		t.Error("HTTP status code is empty")
+	}
+}
